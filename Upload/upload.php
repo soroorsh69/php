2cf8db209ad7a8d9ea772e6aca556fdf4ab785fa
Soroor sheibani <seo.sheibani@gmail.com>
2023-02-12 12:13:37 +0330

    Add all files

--- Upload/upload.php
+++ Upload/upload.php
@@ -1,73 +1,101 @@
 <?php
 if(isset($_FILES['file'])) {
 
-    $file = $_FILES['file'];
-    $name = $file['name'];
-    $tmp_name = $file['tmp_name'];
-    $error = $file['error'];
-    $size = $file['size'];
+    $name = $_FILES['file']['name'];
+ $tmp_name = $_FILES['file']['tmp_name'];
+      $error = $_FILES['file']['error'];
+        $size = $_FILES['file']['size'];
+
+ $folder="Upload/Image".$tmp_name;
 
 
-    $file_ext = explode('.', $name);
-   var_dump($_FILES['image']['error']);die;
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = 'root';
+    $db_db = 'Soroor_db';
 
-    if ($_FILES['image']['error']!==UPLOAD_ERR_OK) {
+    $mysqli = @new mysqli(
+        $db_host,
+        $db_user,
+        $db_password,
+        $db_db
+    );
 
-        switch ($file) {
+    if ($mysqli->connect_error) {
+        echo 'Errno: ' . $mysqli->connect_errno;
+        echo '<br>';
+        echo 'Error: ' . $mysqli->connect_error;
+        exit();
+    }
 
-            case UPLOAD_ERR_INI_SIZE:
+    $sql="INSERT INTO `project_tb`(`project-name`,`image`) VALUES ('$tmp_name','$folder')";
+    $result=mysqli_query( $mysqli, $sql);
 
-              echo  $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+    if (move_uploaded_file($tmp_name, $folder)) {
 
-                break;
+        $msg = "Image uploaded successfully";
 
-            case UPLOAD_ERR_FORM_SIZE:
+    }else{
 
-                echo  $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
-
-                break;
-
-            case UPLOAD_ERR_PARTIAL:
-
-                echo   $message = "The uploaded file was only partially uploaded";
-
-                break;
-
-            case UPLOAD_ERR_NO_FILE:
-
-                echo    $message = "No file was uploaded";
-
-                break;
-
-            case UPLOAD_ERR_NO_TMP_DIR:
-
-                echo    $message = "Missing a temporary folder";
-
-                break;
-
-            case UPLOAD_ERR_CANT_WRITE:
-
-                echo  $message = "Failed to write file to disk";
-
-                break;
-
-            case UPLOAD_ERR_EXTENSION:
-
-                echo     $message = "File upload stopped by extension";
-
-                break;
-
-
-            default:
-
-                echo   $message = "Unknown upload error";
-
-                break;
-
-        }
-
+        $msg = "Failed to upload image";
 
     }
 
 
+//   switch ($error) {
+//
+//            case UPLOAD_ERR_INI_SIZE:
+//
+//              echo  $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+//
+//                break;
+//
+//            case UPLOAD_ERR_FORM_SIZE:
+//
+//                echo  $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+//
+//                break;
+//
+//            case UPLOAD_ERR_PARTIAL:
+//
+//                echo   $message = "The uploaded file was only partially uploaded";
+//
+//                break;
+//
+//            case UPLOAD_ERR_NO_FILE:
+//
+//                echo    $message = "No file was uploaded";
+//
+//                break;
+//
+//            case UPLOAD_ERR_NO_TMP_DIR:
+//
+//                echo    $message = "Missing a temporary folder";
+//
+//                break;
+//
+//            case UPLOAD_ERR_CANT_WRITE:
+//
+//                echo  $message = "Failed to write file to disk";
+//
+//                break;
+//
+//            case UPLOAD_ERR_EXTENSION:
+//
+//                echo     $message = "File upload stopped by extension";
+//
+//                break;
+//
+//
+//            default:
+//
+//                echo   $message = "Unknown upload error";
+//
+//                break;
+//
+//        }
+
+
+
 }
+?>
